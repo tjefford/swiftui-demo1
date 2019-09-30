29895333162e14b743592b8860fbd82eccedc04d
Tyler Jefford <tylerjefford@gmail.com>
2019-09-29 23:45:29 -0500

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Tyler Jefford on 9/29/19.
+//  Copyright © 2019 tyler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
